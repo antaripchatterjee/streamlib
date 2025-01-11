231cac9d365ff703e25cdc87ddcb2e950037d570
Antarip Chatterjee <71959135+antaripchatterjee@users.noreply.github.com>
2025-01-11 20:45:31 +0530

    resturctured filter function logic (#9)

--- includes/stream.h
+++ includes/stream.h
@@ -3,8 +3,19 @@
 
 #include "pch.h"
 
+#if !defined(MINALLOCSIZE)
+#define MINALLOCSIZE 1024 
+#endif // MINALLOCSIZE
+
+#include <stdio.h>
+
+#define NEAREST_DIVISIBLE(MSZ, IZ) (((IZ) == 0) ? -1 : (((MSZ) % (IZ) == 0) ? (MSZ) : ((MSZ) + ((IZ) - ((MSZ) % (IZ))))))
+
+
 enum stream_state_t {
-    SS_INIT = -1,
+    SS_ERR = -1,
+    SS_INIT,
+    SS_ALLOC,
     SS_SET,
     SS_INUSE,
     SS_USED
@@ -13,29 +24,21 @@ enum stream_state_t {
 
 struct stream_t {
     void* items;
-    size_t length;
     const size_t item_size;
+    size_t length;
+    size_t capacity;
     enum stream_state_t state;
-    void(*call)(char*, const size_t, void*);
 };
 
-#define new_stream_t(_T) ({ \
-    void call(char* item_ptr, const size_t index, void* cb_ptr) { \
-        ((void(*)(_T, const size_t))cb_ptr)(*(_T*) item_ptr, index) ;\
-    }\
-    struct stream_t init() { \
-        return (struct stream_t){NULL, 0L, sizeof(_T), SS_INIT, call}; \
-    } \
-    init; \
-})
+#define init_stream_t(_ISZ) ((struct stream_t) { NULL, _ISZ, 0UL, 0UL, SS_INIT })
 
 #if defined(__cplusplus)
 extern "C" {
 #endif // __cplusplus
 
-void make_stream(struct stream_t* stream, void* data_ptr, size_t data_size);
+int make_stream(struct stream_t* stream, void* data_ptr, size_t alloc_size);
 void cleanup_stream(struct stream_t* stream);
-char* next_item_from_stream(struct stream_t* stream);
+void* next_item_from_stream(struct stream_t* stream, void* item_ptr);
 void for_each(struct stream_t* stream, void* cb_ptr);
 
 #if defined(__cplusplus)
--- src/map.c
+++ src/map.c
@@ -1,20 +1,20 @@
 #include "map.h"
 
 void map(struct map_t* this, struct stream_t* stream, void* cb_ptr) {
-    size_t index = 0;
-    stream->state = SS_INUSE;
-    this->data.items = malloc(stream->length * stream->item_size);
-    this->data.length = stream->length;
-    while(stream->length) {
-        char* item_ptr = next_item_from_stream(stream);
-        if(item_ptr) {
-            void* cb_res_ptr = this->data.items + index * this->data.item_size;
-            this->apply(item_ptr, (const size_t) index, cb_ptr, cb_res_ptr);
-            free(item_ptr);
-            index++;
-        }
-    }
-    if(index > 0L) {
-        this->data.state = SS_SET;
-    }
+    // size_t index = 0;
+    // stream->state = SS_INUSE;
+    // this->data.items = malloc(stream->length * stream->item_size);
+    // this->data.length = stream->length;
+    // while(stream->length) {
+    //     char* item_ptr = next_item_from_stream(stream);
+    //     if(item_ptr) {
+    //         void* cb_res_ptr = this->data.items + index * this->data.item_size;
+    //         this->apply(item_ptr, (const size_t) index, cb_ptr, cb_res_ptr);
+    //         free(item_ptr);
+    //         index++;
+    //     }
+    // }
+    // if(index > 0L) {
+    //     this->data.state = SS_SET;
+    // }
 }
